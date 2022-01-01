913684897c5174ce209ef1f523ea50e21d397c29
iroan <2654189525@qq.com>
2022-01-01 22:37:39 +0800

    update code

--- day1/code/helper.sh
+++ day1/code/helper.sh
@@ -18,18 +18,38 @@ function envDiff() {
     # echo $$
 }
 
+function umountRootFS() {
+    cd $START_DIR/myroot
+    sudo umount --quiet proc
+    sudo umount --quiet sys
+    sudo umount --quiet tmp
+    sudo umount --quiet -R dev
+    sudo umount --quiet run
+}
+
 function miniDocker() {
     cd $START_DIR
     rm -f *.out
     gcc -Og -o miniDocker.out mini-docker.c
     # sethostname need super power
+    umountRootFS
+
+    cd $START_DIR
     sudo ./miniDocker.out
+    umountRootFS
     # showIPC
 }
 
-function debugCgroup(){
-    mount -t cgroup    
-    cd /sys/fs/cgroup/cpu/
-    sudo mkdir wkx-demo
+function debug(){
+    # sudo apt search debootstrap
+    # sudo apt install debootstrap
+    # debootstrap --help
+    rm -f *.patch
+    git format-patch -5
+    # git format-patch --help
+    exit 0
+    arch
+    ls /usr/share/debootstrap/scripts
+    sudo debootstrap --no-check-gpg --verbose --arch amd64 bullseye myroot https://mirrors.tuna.tsinghua.edu.cn/debian/
 }
 $@
--- day1/code/mini-docker.c
+++ day1/code/mini-docker.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "utils.h"
-
 #define STACK_SIZE (1024 * 1024)
 
 static char container_stack[STACK_SIZE];
@@ -20,7 +20,29 @@ int container_main(void *arg) {
   if (sethostname(cname, strlen(cname)) != 0) {
     INFO("sethostname failed\n");
   }
-  system("mount -t proc proc /proc");
+
+  if (mount("proc", "myroot/proc", "proc", 0, NULL) != 0) {
+    perror("proc");
+  }
+  if (mount("sysfs", "myroot/sys", "sysfs", 0, NULL) != 0) {
+    perror("sysfs");
+  }
+  if (mount("none", "myroot/tmp", "tmpfs", 0, NULL) != 0) {
+    perror("tmp");
+  }
+  if (mount("udev", "myroot/dev", "devtmpfs", 0, NULL) != 0) {
+    perror("dev");
+  }
+  if (mount("shm", "myroot/dev/shm", "tmpfs", 0, NULL) != 0) {
+    perror("dev/shm");
+  }
+  if (mount("tmpfs", "myroot/run", "tmpfs", 0, NULL) != 0) {
+    perror("run");
+  }
+
+  if (chdir("myroot") != 0 || chroot("./") != 0) {
+    perror("chdir/chroot");
+  }
   execv(container_args[0], container_args);
   INFO("wrong\n");
   return 1;
@@ -28,7 +50,7 @@ int container_main(void *arg) {
 
 int main(int argc, char const *argv[]) {
   INFO("parent\n");
-  int container_pid = clone(container_main, container_stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWIPC | SIGCHLD, NULL);
+  int container_pid = clone(container_main, container_stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWIPC | SIGCHLD, NULL);
   waitpid(container_pid, NULL, 0);
   INFO("stop\n");
   return 0;
