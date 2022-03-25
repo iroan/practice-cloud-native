e048acdfd0101b75cd5a1385e36faf288eba8954
wangkaixuan <2654189525@qq.com>
2022-03-26 01:59:32 +0800

    feat: use nfs, add foreach

--- helper.sh
+++ helper.sh
@@ -1,8 +1,17 @@
-set -x
-
 function restart() {
     # docker-compose down -v
     docker-compose up -d
 }
 
+function tag(){
+    # docker tag nginx:1.21.6-alpine iroan/nginx:v1
+    docker push iroan/nginx:v1
+}
+
+function foreach(){
+    local machines="192.168.50.20 192.168.50.21 192.168.50.22 192.168.50.10 192.168.50.11 192.168.50.40 192.168.50.41 192.168.50.42"
+    for item in $machines; do
+        ssh root@$item apt install nfs-common -y
+    done
+}
 $@
